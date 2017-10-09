a03dc4a85a94e340d0bf56479bba265a6e4b4738
leibin <814289004@qq.com>
2017-10-09 17:01:11 +0800

    add RedBlackBST put method

--- src/searching/RedBlackBST.java
+++ src/searching/RedBlackBST.java
@@ -18,6 +18,42 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 		return x.N;
 	}
 	
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+		root.color = BLACK;
+	}
+	
+	private Node put(Node h, Key key, Value val) {
+		if (h == null) {
+			return new Node(key, val, 1, RED);
+		}
+		
+		int cmp = key.compareTo(h.key);
+		
+		if (cmp < 0) {
+			h.left = put(h.left, key, val);
+		} else if (cmp > 0) {
+			h.right = put(h.right, key, val);
+		} else {
+			h.val = val;
+		}
+		
+		if (!isRed(h.left) && isRed(h.right)) {
+			rotateLeft(h);
+		}
+		
+		if (isRed(h.left) && isRed(h.left.left)) {
+			rotateRight(h);
+		}
+		
+		if (isRed(h.left) && isRed(h.right)) {
+			flipColors(h);
+		}
+		
+		h.N = 1+size(h.left)+size(h.right);
+		return h;
+	}
+	
 	private Node rotateLeft(Node h) {
 		Node x = h.right;
 		h.right = x.left;
