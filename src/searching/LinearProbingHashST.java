ed124171f72dc0667b929b758dff6440e3715ffe
leibin <814289004@qq.com>
2017-10-18 20:17:35 +0800

    add SeparateChainingHashST LinearProbingHashST

---
+++ src/searching/LinearProbingHashST.java
@@ -0,0 +1,87 @@
+package searching;
+
+public class LinearProbingHashST<Key, Value> {
+	private int N;
+	private int M;
+	private Key[] keys;
+	private Value[] vals;
+	
+	public LinearProbingHashST() {
+		keys = (Key[])new Object[M];
+		vals = (Value[])new Object[M];
+	}
+	
+	private int hash(Key key) {
+		return (key.hashCode()&0x7fffffff) % M;
+	}
+	
+	private void resize(int size) {
+		
+	}
+	
+	public void put(Key key, Value val) {
+		
+		if (N >= M/2) {
+			resize(2*M);
+		}
+		int i;
+		
+		for (i = hash(key);keys[i]!=null;i=(i+1)%M) {
+			if (keys[i].equals(key)) {
+				vals[i] = val;
+				return;
+			}
+		}
+		
+		keys[i] = key;
+		vals[i] = val;
+		
+		N++;
+	}
+	
+	public Value get(Key key) {
+		
+		for (int i = hash(key); keys[i]!=null; i=(i+1)%M) {
+			if (keys[i].equals(key)) {
+				return vals[i];
+			}
+		}
+		
+		return null;
+	}
+	
+	private boolean contains(Key key) {
+		return get(key) != null;
+	}
+	
+	public void delete(Key key) {
+		
+		if (!contains(key)) return;
+		int i = hash(key);
+		
+		while (!key.equals(keys[i])) {
+			i=(i+1)%M;
+		}
+		
+		keys[i] = null;
+		vals[i] = null;
+		i=(i+1)%M;
+		
+		while (keys[i] != null) {
+			Key keyToRedo = keys[i];
+			Value valToRedo = vals[i];
+			
+			keys[i] = null;
+			vals[i] = null;
+			N--;
+			
+			put(keyToRedo, valToRedo);
+			i=(i+1)%M;
+		}
+		N--;
+		
+		if (N>0 && N==M/8) {
+			resize(M/2);
+		}
+	}
+}
