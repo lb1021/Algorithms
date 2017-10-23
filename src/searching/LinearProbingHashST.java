ef5eb9780bc2ad2c3004526304d4fabfb68ddcf9
leibin <814289004@qq.com>
2017-10-23 19:49:01 +0800

    add resize() method

--- src/searching/LinearProbingHashST.java
+++ src/searching/LinearProbingHashST.java
@@ -2,21 +2,35 @@ package searching;
 
 public class LinearProbingHashST<Key, Value> {
 	private int N;
-	private int M;
+	private static int M;
 	private Key[] keys;
 	private Value[] vals;
 	
 	public LinearProbingHashST() {
-		keys = (Key[])new Object[M];
-		vals = (Value[])new Object[M];
+		this(M);
+	}
+	
+	public LinearProbingHashST(int cap) {
+		keys = (Key[])new Object[cap];
+		vals = (Value[])new Object[cap];
 	}
 	
 	private int hash(Key key) {
 		return (key.hashCode()&0x7fffffff) % M;
 	}
 	
-	private void resize(int size) {
+	private void resize(int cap) {
+		LinearProbingHashST<Key, Value> t;
+		t = new LinearProbingHashST<Key, Value>(cap);
 		
+		for (int i=0; i<M; i++) {
+			if (keys[i] != null) {
+				t.put(keys[i], vals[i]);
+			}
+		}
+		keys = t.keys;
+		vals = t.vals;
+		M = t.M;
 	}
 	
 	public void put(Key key, Value val) {
