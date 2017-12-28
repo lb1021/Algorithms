352027afff2c1d7a98fed5322604370467671432
leibin <814289004@qq.com>
2017-12-28 13:30:09 +0800

    add Brute-force substring search

---
+++ src/strings/substringsearch/BruteForce.java
@@ -0,0 +1,20 @@
+package strings.substringsearch;
+
+public class BruteForce {
+	public static int search(String pat, String txt) {
+		int M = pat.length();
+		int N = txt.length();
+		for (int i = 0; i <= N - M; i++) {
+			int j;
+			for (j = 0; j < M; j++) {
+				if (txt.charAt(i + j) != pat.charAt(j)) {
+					break;
+				}
+			}
+			if (j == M) {
+				return i;
+			}
+		}
+		return N;
+	}
+}
